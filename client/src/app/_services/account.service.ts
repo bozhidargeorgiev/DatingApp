a74b38ef62767fc1218f47fe40d5a08cb0a66a53
Bozhidar Georgiev <bozhidar@winwin.ie>
2024-01-31 05:18:17 +0000

    End of Section 5

---
+++ client/src/app/_services/account.service.ts
@@ -0,0 +1,55 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, map } from 'rxjs';
+import { User } from '../_models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+
+  baseUrl = "https://localhost:7220/api/";
+  private currentUserSource = new BehaviorSubject<User | null>(null);
+  currentUser$ = this.currentUserSource.asObservable();
+
+  constructor(private http: HttpClient) { 
+    
+  }
+
+  // Login
+  login(model: any) {
+    return this.http.post<User>(this.baseUrl + "account/login", model).pipe(
+      map((response: User) => {
+        const user = response;
+
+        if (user) {
+          localStorage.setItem("user", JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+        return user;
+      })
+    )
+  }
+
+  register(model: any) {
+    return this.http.post<User>(this.baseUrl + "account/register", model).pipe(
+      map(user => {
+        if (user) {
+          localStorage.setItem("user", JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+      })
+    )
+  }
+
+  setCurrentUser(user: User) {
+    this.currentUserSource.next(user);
+  }
+
+  // Logout
+  logout() {
+    localStorage.removeItem("user");
+    this.currentUserSource.next(null);
+  }
+
+}
